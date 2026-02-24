dd46381c10472f5abdb6e91bce4d1ebd131d376c
Suraj Singh <www.sr2006@gmail.com>
2026-02-24 14:25:58 +0000

    Increament and Decreament

---
+++ 01_basics/01_increament-decreament.js
@@ -0,0 +1,11 @@
+let gameNumber = 5
+
+// Postfix
+// let postincre = gameNumber++
+// console.log(postincre)   // output = 5
+// console.log(gameNumber)  // output = 6
+
+// Prefix
+let priincre = ++gameNumber
+console.log(priincre)     // output = 6
+console.log(gameNumber)   // output = 6
