26f233f02981a76fa63c9206e1fa6f81c71340cc
Suraj Singh <www.sr2006@gmail.com>
2026-02-24 12:41:49 +0000

    Datatypes

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,21 @@
+"use strict"  //it's means, your code treated newer version
+
+// alert(3+3) // it's use in vs code, not browser
+
+/* datatype of type
+ *number => 2 to power 53
+ */
+
+ let number = 496
+ let string = "suraj"
+ let bigInt = 49875282
+ let boolean = true
+ console.log(typeof null)
+ console.log(typeof undefined)
+ console.log(typeof number)
+ console.log(typeof string)
+ console.log(typeof bigInt)
+ console.log(typeof boolean)
+ console.log(typeof symbol)  //unique value
+ console.log(24/+0)
+ console.log(24/-0)
