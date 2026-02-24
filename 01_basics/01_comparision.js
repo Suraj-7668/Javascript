daba545d64b23236406176e2a2511e3cf0548de8
Suraj Singh <www.sr2006@gmail.com>
2026-02-24 19:07:52 +0000

    Comparision

---
+++ 01_basics/01_comparision.js
@@ -0,0 +1,26 @@
+console.log(2>1)
+console.log(2<1)
+console.log(2>=1)
+console.log(2<=1)
+console.log(2!=1)
+
+
+console.log("2">1)
+console.log("02"<1)
+
+console.log(null < 0)
+console.log(null > 0)
+console.log(null <= 0)
+console.log(null >= 0)
+
+console.log(undefined < 0)
+console.log(undefined > 0)
+console.log(undefined <= 0)
+console.log(undefined >= 0)
+
+// this type comparision is avoid because it's create compusion
+
+// Strict comparision ===
+// in this comparision value and datatypes 
+
+console.log("2"===1)
