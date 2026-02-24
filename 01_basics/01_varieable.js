3ef283d536fc14c543f509565634dd766c1c821e
Suraj Singh <www.sr2006@gmail.com>
2026-02-24 12:38:53 +0000

    Varieables

---
+++ 01_basics/01_varieable.js
@@ -0,0 +1,15 @@
+// constant varieable
+const accountId = 39480
+
+// local varieable  " let"
+let accountEmail = "sajkf@gmail.com"
+
+// local varieable " var"
+var accountPassward = "34786985"
+
+// but var not use for create varieable, because scope issue
+
+console.log(accountId)
+
+// table print
+console.table([accountId, accountEmail, accountPassward,])
