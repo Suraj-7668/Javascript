91f82231075c5145d27b06a8105c1348483a3215
Suraj Singh <www.sr2006@gmail.com>
2026-02-24 12:44:52 +0000

    Conversion and Operator

---
+++ 01_basics/01_conversion-operator.js
@@ -0,0 +1,46 @@
+let score = "39fd"
+
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// Conversion of datatypes
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "39" => 33
+// null => 0
+// "39fd" => NaN
+// true => 1; false => 0
+// " " => 0
+
+let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ************* Operations & Operator ************
+
+let str1 = "Hello"
+let str2 = "Suraj"
+let str3 = str1+str2
+console.log(str3)
+
+console.log(5+5)
+console.log(5-5)
+console.log(5*5)
+console.log(5**5)
+console.log(5/2)
+console.log(5%5)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+3)
+console.log(1+2+"3")
