b092e83847902dc38d3d364535ede466fdbc5fdf
Suraj Singh <www.sr2006@gmail.com>
2026-03-14 05:06:24 +0000

    Object Destructor

---
+++ 02_basics/04_object_destructure.js
@@ -0,0 +1,24 @@
+// Object de structure
+const User = {
+    course_name: "javascript",
+    price: 999,
+    email: "s@gmail.com"
+}
+
+const {course_name: course} = User
+console.log(course)
+
+// JSON and API
+
+// JSON Syntax
+// {
+//     "name": "Suraj",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {} 
+]
