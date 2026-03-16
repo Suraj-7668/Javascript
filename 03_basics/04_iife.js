b1ca30453e82ebfbaff9bf77479fbad4c8c82a0d
Suraj Singh <www.sr2006@gmail.com>
2026-03-16 15:47:17 +0000

    iife create

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,16 @@
+// function one(){
+//     // regular function
+//     console.log("Suraj")
+// }
+// one()
+
+// iffe create => ()()  first parathensices is functon definition and second parathensices 'execution'
+// when pass arrgument in second parathensices ()
+
+(function two(){
+    console.log(`"Suraj Singh"`)
+})();
+
+((subname) => {
+    console.log(`Suraj ${subname} rajput `)
+})("Singh")
