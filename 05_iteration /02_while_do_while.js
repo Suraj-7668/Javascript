ecc1e89660958c822d4ea94a94e53c9dd037704a
Suraj Singh <www.sr2006@gmail.com>
2026-03-19 10:06:23 +0000

    While and do-while Loop

---
+++ 05_iteration /02_while_do_while.js
@@ -0,0 +1,20 @@
+// let score = 2
+// while(score < 30){
+//     console.log(`"this is a valied condition ${score}"`)
+//     score = score+2
+// }
+
+const  myArray = ["Sachin tendukar","Kapli Sharma","Virat Kohli", "Rohit Sharma"]
+let arr = 0
+while(arr < myArray.length){
+    console.log(myArray[arr]);                             
+    arr = arr+1
+}
+
+// do while loop Syntax
+
+let score = 22
+do{
+    console.log("At least one run execute loop")
+    score++
+}while(score < 20)
